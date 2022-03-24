4f8f879bfbb6b0d04f4186f7866e03161db8b90b
Nisha Bhavsar <101039209+nisha-1712@users.noreply.github.com>
2022-03-24 14:41:40 +0530

    String methods

---
+++ src/src/Main.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String a ="NISHA";
+    String b ="vasu";
+    System.out.println(a.toLowerCase());
+    System.out.println(a.toUpperCase());
+
+    System.out.println(a.concat(b));
+    System.out.println(b.length());
+
+    String c="   Bhavsar   ";
+    System.out.println(c.trim());
+    String d="";
+    System.out.println(d.isEmpty());
+
+    }
+}
